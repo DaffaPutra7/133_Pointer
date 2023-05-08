2f2bcbef9b22f87cef91d1bb4198d998db939302
DaffaPutra7 <putradika3604@gmail.com>
2023-05-08 11:32:51 +0700

    Tambah Void Siswa dan Void Orang

--- FriendKelas.cpp/FriendKelas.cpp.cpp
+++ FriendKelas.cpp/FriendKelas.cpp.cpp
@@ -14,5 +14,15 @@ public:
 class siswa {
 private:
 	int id;
-
+public:
+	void setId(int pId);
+	void displayAll(orang& a);
 };
+
+void siswa::displayAll(orang& a) {
+	cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+
